112fa38e6ece3415162cf62f93ab65ad2b98c670
El El <elel@3wh.net>
2016-11-29 14:13:59 +0300

    Fix return type

--- src/api/quotes_level_2.cpp
+++ src/api/quotes_level_2.cpp
@@ -2,24 +2,24 @@
 
 bool qlua::api::Subscribe_Level_II_Quotes(const char* class_code, const char* sec_code) {
   const char api_name[] = "Subscribe_Level_II_Quotes";
-  typedef std::tuple<int> return_type;
+  typedef std::tuple<bool> return_type;
   auto params = std::make_tuple(class_code, sec_code);
   auto rslt = lua_.pcall<return_type>(api_name, params);
-  return std::get<0>(rslt) == 1;
+  return std::get<0>(rslt);
 }
 
 bool qlua::api::Unsubscribe_Level_II_Quotes(const char* class_code, const char* sec_code) {
   const char api_name[] = "Unsubscribe_Level_II_Quotes";
-  typedef std::tuple<int> return_type;
+  typedef std::tuple<bool> return_type;
   auto params = std::make_tuple(class_code, sec_code);
   auto rslt = lua_.pcall<return_type>(api_name, params);
-  return std::get<0>(rslt) == 1;
+  return std::get<0>(rslt);
 }
   
 bool qlua::api::IsSubscribed_Level_II_Quotes(const char* class_code, const char* sec_code) {
   const char api_name[] = "IsSubscribed_Level_II_Quotes";
-  typedef std::tuple<int> return_type;
+  typedef std::tuple<bool> return_type;
   auto params = std::make_tuple(class_code, sec_code);
   auto rslt = lua_.pcall<return_type>(api_name, params);
-  return std::get<0>(rslt) == 1;
+  return std::get<0>(rslt);
 }
