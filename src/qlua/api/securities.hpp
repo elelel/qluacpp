46645b6a196cb9fde6146832f4887b21e0fb6aa7
El El <elel@3wh.net>
2017-05-25 10:53:56 +0000

    Fix securities api

--- src/qlua/api/securities.hpp
+++ src/qlua/api/securities.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 #include "../structs/function_results.hpp"
-/*
+
 // getClassesList - функция для получения списка кодов классов, полученных с сервера в ходе сеанса связи
 QLUACPP_DETAIL_API_FUNCTION2(const char*, getClassesList)
 // getClassInfo - функция для получения информации о классе
-QLUACPP_DETAIL_API_FUNCTION_RES1_APPLY4(::qlua::table::class_info_getClassesInfo,
+QLUACPP_DETAIL_API_FUNCTION_RES1_APPLY4(::qlua::table::class_info_getClassInfo,
                                         getClassInfo,
                                         const char*, class_name
                                         )
@@ -14,4 +14,4 @@ QLUACPP_DETAIL_API_FUNCTION_RES1_APPLY4(::qlua::table::class_info_getClassesInfo
 QLUACPP_DETAIL_API_FUNCTION4(const char*,
                              getClassSecurities,
                              const char*, class_name)
-*/
+
