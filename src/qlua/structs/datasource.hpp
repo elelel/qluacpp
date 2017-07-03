df3d39fe8788b11e9b0b6731a010705461bc5675
El El <elel@3wh.net>
2017-07-03 13:40:37 +0000

    Datasource in working state

--- src/qlua/structs/datasource.hpp
+++ src/qlua/structs/datasource.hpp
@@ -24,48 +24,51 @@ namespace qlua {
                 const unsigned int interval, // интервал запрашиваемого графика, INTERVAL_*
                 const char* param = nullptr // Необязательный параметр (что запрашивать)
                 ) : l_(l) {
-      auto set_pointers = [this] (const lua::state& s) -> int {
-        if (s.istable(-2)) {
-          // Copy object table
-          s.getglobal(desc_table_name().c_str());
-          if (s.isnil(-1)) {  // Create table if doesn't exist
-            s.pop(1);
-            s.newtable();
-            s.setglobal(desc_table_name().c_str());
-            s.getglobal(desc_table_name().c_str());
-          }
-          if (s.istable(-1)) {
-            // Source table at -3, dest table at -1
-            s.pushnil(); // Push nil as key
-            while (s.next(-4) != 0) {
-              // Duplicate the key
-              s.pushvalue(-2);
-              // Swap the key duplicate with value
-              s.insert(-2);
-              // Dest table is now at -4, set key/value
-              s.settable(-4);
+      auto f = [this] (const lua::state& l) -> int {
+        // Check if CreateDataSource returned a table in first rslt
+        if (l_.istable(-2)) {
+          // Copy the table
+          l_.pushvalue(-2);
+          // Create reference
+          if (l_.istable(-4)) {
+            ref_key_ = luaL_ref(l_.C_state(), -4);
+            if (ref_key_ != LUA_NOREF) {
+              l_.pop(2);
+              return 0;
+            } else {
+              l_.pop(1); // pushvalue
+              throw std::runtime_error("Failed to create reference to datasource");
             }
-            s.pop(); // getglobal (dest table)
           } else {
-            s.pop(1 // getglobal (dest table)
-                  + 2 // values of function result on stack after pcall);
-                  );
-            throw std::runtime_error("Failed to create lua table in globals for datasource descriptor");
           }
-          return 2;
-        } else {
-          auto err = s.at<const char*>(-1);
+        } else  {
+          auto err = l_.at<const char*>(-1);
           std::string msg("Failed to create datasource, qlua returned: ");
           msg += err();
-          s.pop(2);
+          l_.pop(2);
           throw std::runtime_error(msg.c_str());
         }
+        return 0;
       };
-
-      if (param != nullptr) {
-        l_.call_and_apply(set_pointers, 2, "CreateDataSource", class_code, sec_code, interval, param);
+      
+      // Take references descriptor table
+      l_.getglobal(desc_table_name().c_str());
+      if (l_.isnil(-1)) {  // Create table if doesn't exist
+        l_.pop(1);
+        l_.newtable();
+        l_.setglobal(desc_table_name().c_str());
+        l_.getglobal(desc_table_name().c_str());
+      }
+      if (l_.istable(-1)) {
+        // Call CreateDataSource and handle the results
+        if (param != nullptr) {
+          l_.call_and_apply(f, 2, "CreateDataSource", class_code, sec_code, interval, param);
+        } else {
+          l_.call_and_apply(f, 2, "CreateDataSource", class_code, sec_code, interval);
+        }
       } else {
-        l_.call_and_apply(set_pointers, 2, "CreateDataSource", class_code, sec_code, interval);
+        l_.pop(1); // getglobal desc table
+        throw std::runtime_error("Failed to create lua table in globals for datasource descriptor");
       }
     }
 
@@ -91,44 +94,67 @@ namespace qlua {
     
     // Time
     time T(const unsigned int idx) const {
+      if (ref_key_ == LUA_NOREF)
+        throw std::runtime_error("Can't call datasource method: no reference to the object");
+
       l_.getglobal(desc_table_name().c_str());
+      auto i = 1;
       if (l_.istable(-1)) {
+        l_.pushnumber(ref_key_);
+        l_.rawget(-2);
+        ++i;
         l_.pushstring("T");
-        l_.gettable(-1); // Push function from table to stack
-        l_.pcall(1, 1, 0);
-        auto t = l_.at<table::chart_time>(-1)();
-        time rslt =  {t.year(), t.month(), t.day(), t.week_day(), t.hour(), t.min(), t.sec(), t.ms(), t.count()};
-        l_.pop(2); // table and value
-        return rslt;
+        l_.rawget(-2); // Push function from table to stack
+        ++i;
+        if (l_.isfunction(-1)) {
+          l_.pushvalue(-2); // Push 'this'
+          l_.pushnumber(idx);
+          l_.pcall(2, 1, 0);
+          ++i;
+          auto t = l_.at<table::chart_time>(-1)();
+          time rslt =  {t.year(), t.month(), t.day(), t.week_day(), t.hour(), t.min(), t.sec(), t.ms(), t.count()};
+          l_.pop(i);
+          return rslt;
+        } else {
+          l_.pop(i);
+          throw std::runtime_error("Call to datasource method failed: T method is not a function in object table");
+        }
       } else {
-        l_.pop(1); // getglobal
+        l_.pop(i); 
         throw std::runtime_error("Call to datasource method failed: " + desc_table_name() + " is not a table in lua globals");
       }
     }
 
     bool SetUpdateCallback(const char* lua_function_name) {
+      if (ref_key_ == LUA_NOREF)
+        throw std::runtime_error("Can't call datasource method: no reference to the object");
+
       l_.getglobal(desc_table_name().c_str());
       auto i = 1;
       if (l_.istable(-1)) {
+        l_.pushnumber(ref_key_);
+        l_.rawget(-2);
+        ++i;
         l_.pushstring("SetUpdateCallback");
         l_.rawget(-2); // Push function from table to stack
         ++i;
-        if (!l_.isnil(-1)) {
+        if (l_.isfunction(-1)) {
+          l_.pushvalue(-2); // Push 'this'
+          ++i;
           l_.getglobal(lua_function_name);
           ++i;
           if (l_.isfunction(-1)) {
-            l_.pcall(1, 1, 0);
+            l_.pcall(2, 1, 0);
             ++i;
             auto rslt = l_.at<bool>(-1)();
-            l_.pop(i);
+            l_.pop(i - 3); // Function address and arguments
             return rslt;
           } else {
-            l_.pop(i);
             throw std::runtime_error("Call to datasource method failed: " + std::string(lua_function_name) + " is not a function in lua globals");
           }
         } else {
           l_.pop(i);
-          throw std::runtime_error("Call to datasource method failed: SetUpdateCallback method is nil in desc table");
+          throw std::runtime_error("Call to datasource method failed: SetUpdateCallback method is not a function in object table");
         }
       } else {
         l_.pop(i); 
@@ -137,71 +163,135 @@ namespace qlua {
     }
 
     bool SetEmptyCallback() {
+      if (ref_key_ == LUA_NOREF)
+        throw std::runtime_error("Can't call datasource method: no reference to the object");
+
       l_.getglobal(desc_table_name().c_str());
+      auto i = 1;
       if (l_.istable(-1)) {
+        l_.pushnumber(ref_key_);
+        l_.rawget(-2);
+        ++i;
         l_.pushstring("SetEmptyCallback");
-        l_.rawget(-1); // Push function from table to stack
-        l_.pcall(0, 1, 0);
-        auto rslt = l_.at<bool>(-1)();
-        l_.pop(2); // table and value
-        return rslt;
+        l_.rawget(-2); // Push function from table to stack
+        ++i;
+        if (l_.isfunction(-1)) {
+          l_.pushvalue(-2); // Push 'this'
+          l_.pcall(0, 1, 0);
+          auto rslt = l_.at<unsigned int>(-1)();
+          l_.pop(i - 1); // Function name already removed
+          return rslt;
+        } else {
+          l_.pop(i);
+          throw std::runtime_error("Call to datasource method failed: SetEmptyCallback method is not a function in object table");
+        }
       } else {
-        l_.pop(1); // getglobal
+        l_.pop(i); 
         throw std::runtime_error("Call to datasource method failed: " + desc_table_name() + " is not a table in lua globals");
       }
     }
 
     unsigned int Size() {
+      if (ref_key_ == LUA_NOREF)
+        throw std::runtime_error("Can't call datasource method: no reference to the object");
+
       l_.getglobal(desc_table_name().c_str());
+      auto i = 1;
       if (l_.istable(-1)) {
+        l_.pushnumber(ref_key_);
+        l_.rawget(-2);
+        ++i;
         l_.pushstring("Size");
-        l_.rawget(-1); // Push function from table to stack
-        l_.pcall(0, 1, 0);
-        auto rslt = l_.at<unsigned int>(-1)();
-        l_.pop(2); // table and value
-        return rslt;
+        l_.rawget(-2); // Push function from table to stack
+        ++i;
+        if (l_.isfunction(-1)) {
+          l_.pushvalue(-2); // Push 'this'
+          l_.pcall(1, 1, 0);
+          ++i;
+          auto rslt = l_.at<unsigned int>(-1)();
+          l_.pop(i - 1); // Function name already removed
+          return rslt;
+        } else {
+          l_.pop(i);
+          throw std::runtime_error("Call to datasource method failed: Size method is not a function in object table");
+        }
       } else {
-        l_.pop(1); // getglobal
+        l_.pop(i); 
         throw std::runtime_error("Call to datasource method failed: " + desc_table_name() + " is not a table in lua globals");
       }
     }
     
     bool Close() {
+      if (ref_key_ == LUA_NOREF)
+        throw std::runtime_error("Can't call datasource method: no reference to the object");
+
       l_.getglobal(desc_table_name().c_str());
+      auto i = 1;
       if (l_.istable(-1)) {
+        l_.pushnumber(ref_key_);
+        l_.rawget(-2);
+        ++i;
         l_.pushstring("Close");
-        l_.rawget(-1); // Push function from table to stack
-        l_.pcall(0, 1, 0);
-        auto rslt = l_.at<bool>(-1)();
-        l_.pop(2); // table and value
-        return rslt;
+        l_.rawget(-2); // Push function from table to stack
+        ++i;
+        if (l_.isfunction(-1)) {
+          l_.pushvalue(-2); // Push 'this'
+          l_.pcall(0, 1, 0);
+          auto rslt = l_.at<unsigned int>(-1)();
+          l_.pop(i - 1); // Function name already removed
+
+          l_.getglobal(desc_table_name().c_str());
+          luaL_unref(l_.C_state(), -1, ref_key_);
+          ref_key_ = LUA_NOREF;
+          l_.pop(1); // desc table
+
+          return rslt;
+        } else {
+          l_.pop(i);
+          throw std::runtime_error("Call to datasource method failed: Close method is not a function in object table");
+        }
       } else {
-        l_.pop(1); // getglobal
+        l_.pop(i); 
         throw std::runtime_error("Call to datasource method failed: " + desc_table_name() + " is not a table in lua globals");
       }
     }
 
-    std::string desc_table_name() const {
-      return "qluacpp_datasource_object_" + std::to_string((uint32_t)this);
+    static std::string& desc_table_name() {
+      static std::string s = "qluacpp_datasource_object_refs";
+      return s;
     }
 
   private:
     lua::state l_;
+    int ref_key_{LUA_NOREF};
 
     inline double double_method_call(const char* name, const unsigned int idx) const {
+      if (ref_key_ == LUA_NOREF)
+        throw std::runtime_error("Can't call datasource method: no reference to the object");
+
       l_.getglobal(desc_table_name().c_str());
       auto i = 1;
       if (l_.istable(-1)) {
-        l_.pushstring(name);
-        l_.rawget(-1); // Push function from table to stack
-        l_.push<unsigned int>(idx);
-        l_.pcall(1, 1, 0);
+        l_.pushnumber(ref_key_);
+        l_.rawget(-2);
         ++i;
-        auto rslt = l_.at<double>(-1)();
-        l_.pop(i);
-        return rslt;
+        l_.pushstring(name);
+        l_.rawget(-2); // Push function from table to stack
+        ++i;
+        if (l_.isfunction(-1)) {
+          l_.pushvalue(-2); // Push 'this'
+          l_.pushnumber(idx);
+          l_.pcall(2, 1, 0);
+          ++i;
+          auto rslt = l_.at<double>(-1)();
+          l_.pop(i - 1);  // Function popped by pcall
+          return rslt;
+        } else {
+          l_.pop(i);
+          throw std::runtime_error("Call to datasource method failed: " + std::string(name) + " method is not a function in object table");
+        }
       } else {
-        l_.pop(i);
+        l_.pop(i); 
         throw std::runtime_error("Call to datasource method failed: " + desc_table_name() + " is not a table in lua globals");
       }
     }
